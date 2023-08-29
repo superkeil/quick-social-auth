f8c37c94926c49ba9335ee58e96c345b696116fe
ACENSI SARL - SUEDILE, Karel <ksuedile@bouyguestelecom.fr>
2023-08-29 03:36:16 +0200

    more tests, 100% coverage

--- test/infrastructure/decorators/user.decorator.spec.ts
+++ test/infrastructure/decorators/user.decorator.spec.ts
@@ -1,9 +1,28 @@
-import { User } from '../../../lib/auth';
+import { User, UserModel } from '../../../lib/auth';
 
 describe('User Decorator', () => {
     it('should exist', () => {
-        const decorator = User();
-        expect(decorator).toBeDefined();
-    });
+        class Custom {
+            public user: UserModel;
+        }
 
+        const ctx = {
+            switchToHttp: () => ({
+                getRequest: () => ({
+                    headers: {
+                        authorization: 'Bearer token'
+                    }
+                })
+            })
+        };
+
+        const decorator = User();
+        const instance = new Custom();
+        decorator(instance, 'user', 0);
+        const metadata = Reflect.getMetadata('__routeArguments__', instance.constructor, 'user');
+        const metadataKey = Object.keys(metadata)[0];
+        const result = metadata[metadataKey].factory(null, ctx);
+
+        expect(result).toBe('token');
+    });
 });
